698e2a8d8d8e7cfd923abbf6544fec63cdb83e4f
Alex3s7s <32075336+alexisliao@users.noreply.github.com>
2018-03-30 18:55:39 -0400

    Add updated register file

--- b20/register.php
+++ b20/register.php
@@ -1,24 +1,18 @@
-
 <?php
 /* Registration process, inserts user info into the database */
-include 'config.php';
+include ('config.php');
+
+$firstname =$_POST['firstname'];
+$lastname = $_POST['lastname'];
+$UTORid = $_POST['UTORid'];
+$password = $_POST['password'];
+// should be selection here
+$logintype = $_POST["userselect"];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $_SESSION['UTORid'] = $_POST['UTORid'];
-    $_SESSION['firstname'] = $_POST['firstname'];
-    $_SESSION['lastname'] = $_POST['lastname'];
 
-    // Escape all $_POST variables to protect against SQL injections
-    $firstname = $mysqli->escape_string($_POST['firstname']);
-    $lastname = $mysqli->escape_string($_POST['lastname']);
-    $UTORid = $mysqli->escape_string($_POST['UTORid']);
-    $password = $mysqli->escape_string(password_hash($_POST['password'], PASSWORD_BCRYPT));
-    // should be selection here
-    //$logintype = $mysqli->escape_string( md5( rand(0,1000) ) );
-
-    // Check if user with that email already exists
+    // Check if user with that UTORid already exists
     $result = $mysqli-> query("SELECT * FROM Accounts WHERE UTORid='$UTORid'") or die($mysqli->error());
-
     // We know user email exists if the rows returned are more than 0
     if ( $result->num_rows > 0 ) {
         $_SESSION['message'] = 'User with this UTORid already exists!';
@@ -26,7 +20,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // UTORIid doesn't already exist in a database, proceed...
         $sql = "INSERT INTO Accounts (firstname, lastname, UTORid, password, logintype) " 
                 . "VALUES ('$firstname','$lastname','$UTORid','$password', '$logintype')";
-
         // Add user to the database
         if ( $mysqli->query($sql) ){
             // So we know the user has logged in
@@ -35,7 +28,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $_SESSION['message'] = 'Registration failed!';
             header("location: error.php");
         }
-
     }
 }
 ?>
@@ -57,25 +49,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 				<p>UTORid</p>
 				<input type="text" name="" placeholder="Enter UTORid">
 				<p>Password</p>
-                <input type="text" name="" placeholder="Enter Password">
-
-
+                <input type="text" name="" placeholder="******">
 
                 <p>Login As</p>
                 <script src="js/option.js"></script>
                 <div class="test-box" style="padding: 15px 5px 20px 0px">
-                    <select name="pay1" id="pay1">
+                    <select name="userselect">
                         <option value="1" selected>Student</option>
                         <option value="2">T.A.</option>
                         <option value="3">Instructor</option>
                     </select>
                 </div>
-            
 
-
-
-                <a href="main.php"><button class="button button-block" name="login" />Register</button></a><br>
-				<a href="main.php">Sign In</a>
+                <a href="login.php"><button class="button button-block" name="login" />Register</button></a><br>
+				<a href="login.php">Sign In</a>
 			</form>
 		</div>
 	</body>
