a3be15ec38e5ae7b4b9f9b15701a5175fd7fe6d1
Cyndi Liu <37076311+CyndiLiu@users.noreply.github.com>
2018-03-30 21:58:55 -0400

    Delete db.php

--- login-system/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Database connection settings
-$host = 'localhost';
-$username = 'root';
-$password = 'm';
-$db = 'accounts';
-$mysqli = new mysqli($host,$username,$password,$db) or die($mysqli->error);
-?>
