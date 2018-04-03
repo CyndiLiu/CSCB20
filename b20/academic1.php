d28802007a95a8e77d70ed11d865c5ebaabacf84
Cyndi Liu <37076311+CyndiLiu@users.noreply.github.com>
2018-04-03 18:28:23 -0400

    rebuild html

--- b20/academic1.php
+++ b20/academic1.php
@@ -1,7 +1,7 @@
 
 <html lang="en">
 	<head>
-		<title>test</title>
+		<title>academic</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<link href="main.css" rel="stylesheet" type="text/css"/>
 	</head>
@@ -40,8 +40,8 @@
 						
 						$myutorid = $_SESSION['UTORid'];
 						$mysql = "SELECT quiz1, quiz2, quiz3, midterm, assignment1, assignment2, assignment3, final  
-						FROM Marks WHERE Marks.UTORid = '$myutorid'";
-						$results = mysqli_query($db,$mysql);
+						FROM Mark WHERE Mark.UTORid = '$myutorid'";
+						$result = mysqli_query($db,$mysql);
 						if(mysqli_num_rows($result) > 0){
 							while ($row = mysqli_fetch_assoc($result)) {
 								echo '<tr>
@@ -61,11 +61,32 @@
 			</table>
 			<p><h1>Remark Request</h1></p>
 			<HR style="border:1  black" width="100%" color=black SIZE=1>
+			<select name="logintype" id="type1">
+                <option value="1" selected>Quiz1</option>
+                <option value="2">Quiz2</option>
+                <option value="3">Quiz3</option>
+				<option value="4">Midterm</option>
+				<option value="5">Assignment1</option>
+				<option value="6">Assignment2</option>
+				<option value="7">Assignment3</option>
+				<option value="8">Final</option>
+            </select>
+			<input type="submit" name="" value="Remark">
 
-			<textarea rows="10" cols="50">Comments here. </textarea><br>
+			<p><h1>Feedback</h1></p>
+			<HR style="border:1  black" width="100%" color=black SIZE=1>
+			<p>You can start with......</p>
+				<pre>
+(a) What do you like about the instructor teaching?
+(b) What do you recommand the instructor to do to improve their teaching?
+(c) What do you like about the labs?
+(d) What do you recommand the lab instructors to do to improve their lab teaching?
+(e) ......
+            	</pre>
+			<textarea rows="10" cols="50">Comments here. <	/textarea><br>
 			<button href="academic1.html">Click Me!</button>
 
-            
+             
         </main>
 
 			
